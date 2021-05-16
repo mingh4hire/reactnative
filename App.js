5a2fa98c0256953aad7af978de5f9d13e9121ad1
m <mingh4hire@gmail.com>
2021-05-16 13:05:19 -0500

    asdf

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start
+        asdlf j;lasdfj ;lkasdjf;
+        
+         working on your app!</Text>
+         <button text='asdfsda'/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
